431271b075ac978243e14ed34ac56316e40c57ab
Neeraj Kumar <neeraj1.jnv@gmail.com>
2025-08-13 09:44:14 +0530

    Added new rg-del

--- main.tf
+++ main.tf
@@ -15,3 +15,8 @@ resource "azurerm_storage_account" "sa-patlu" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
+
+resource "azurerm_resource_group" "rg-del" {
+  name     = "rg-del"
+  location = "central India"
+}
